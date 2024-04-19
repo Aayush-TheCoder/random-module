5d1afc851863df85d644258e810b88ca8ac78e80
Coder Aayush <122539113+Aayush-TheCoder@users.noreply.github.com>
2024-04-19 20:25:04 +0530

    Add files via upload

---
+++ random choice.py
@@ -0,0 +1,8 @@
+# random fruits
+
+import random #importing random module
+
+fruits = ["apple", "banana", "grapes", "mango", "pinapple", "watermelon", "orange", "lichi"]
+random_fruits = random.choice(fruits) #random fruits
+
+print(random_fruits)
